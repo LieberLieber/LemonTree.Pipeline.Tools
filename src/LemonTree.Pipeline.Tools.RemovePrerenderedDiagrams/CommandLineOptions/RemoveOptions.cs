dd52fd04fcbff9e2d36d26ce4385ab4c6e550d86
danielsiegl <daniel.siegl@lieberlieber.com>
2022-04-24 22:48:42 +0200

    Implement CommandLineOptions in RemoveDiagrams
    Implement CommandLineOptions

---
+++ src/LemonTree.Pipeline.Tools.RemovePrerenderedDiagrams/CommandLineOptions/RemoveOptions.cs
@@ -0,0 +1,9 @@
+﻿using CommandLine;
+
+namespace LemonTree.Pipeline.Tools.RemovePrerenderedDiagrams.CommandLineOptions
+{
+    [Verb("Remove", HelpText = "Removes the DIAGRAMIMAGEMAP entries from the 'Model' repository.")]
+    internal class RemoveOptions:BaseOptions
+    {
+    }
+}
