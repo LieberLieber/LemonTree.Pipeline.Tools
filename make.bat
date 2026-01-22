9d43edf12a0be352a17c2b8eb26c0455c4f0f4ec
Daniel Siegl <daniel.siegl@syntevo.com>
2026-01-22 15:24:47 +0100

    Add test.ps1 script to run ModelCheck and update make.bat with comment

--- make.bat
+++ make.bat
@@ -3,6 +3,7 @@ REM Build script for LemonTree.Pipeline.Tools solution
 
 echo Building LemonTree.Pipeline.Tools solution...
 dotnet build src\LemonTree.Pipeline.Tools.sln --configuration Release
+REM dotnet publish src\LemonTree.Pipeline.Tools.ModelCheck\LemonTree.Pipeline.Tools.ModelCheck.csproj --configuration Release --runtime win-x86 --self-contained
 
 if %ERRORLEVEL% EQU 0 (
     echo.
