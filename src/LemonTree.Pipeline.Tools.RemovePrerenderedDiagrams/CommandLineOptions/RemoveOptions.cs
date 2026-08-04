4624564b62f35fe216a9a9cb43bf547e2452a1f0
Daniel Siegl <41949368+danielsiegl@users.noreply.github.com>
2026-08-04 11:06:03 +0200

    #92 retire remove prerendered diagrams tool (#93)
    * #92 removed source code and project files
    * #92 removed references to the deleted project from the github workflows

--- src/LemonTree.Pipeline.Tools.RemovePrerenderedDiagrams/CommandLineOptions/RemoveOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommandLine;
-
-namespace LemonTree.Pipeline.Tools.RemovePrerenderedDiagrams.CommandLineOptions
-{
-    [Verb("Remove", HelpText = "Removes the DIAGRAMIMAGEMAP entries from the 'Model' repository.")]
-    internal class RemoveOptions:BaseOptions
-    {
-    }
-}
